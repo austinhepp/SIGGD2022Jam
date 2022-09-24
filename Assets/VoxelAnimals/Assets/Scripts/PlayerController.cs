6b8787d23de13f75090aa9a9549d22b609101692
austinhepp <hepp0@purdue.edu>
2022-09-24 14:35:07 -0400

    fixed snowball growth

--- Assets/VoxelAnimals/Assets/Scripts/PlayerController.cs
+++ Assets/VoxelAnimals/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    [SerializeField] private GameObject snowball;
     public float movementSpeed = 3;
     public float jumpForce = 300;
     public float timeBeforeNextJump = 1.2f;
     private float canJump = 0f;
+    
     Animator anim;
     Rigidbody rb;
     
@@ -29,11 +30,20 @@ public class PlayerController : MonoBehaviour
         float moveVertical = Input.GetAxisRaw("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 maxSnowballSize = new Vector3(2.5f, 2.5f, 2.5f);
 
         if (movement != Vector3.zero)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
             anim.SetInteger("Walk", 1);
+              if (snowball.transform.localScale.x < maxSnowballSize.x && snowball.transform.localScale.y < maxSnowballSize.y && snowball.transform.localScale.z < maxSnowballSize.z) {
+                Vector3 temp = snowball.transform.localScale;
+                temp += new Vector3(0.001f, 0.001f, 0.001f);
+                snowball.transform.localScale = temp;
+                //snowball.transform.localScale = snowball.transform.localScale * 1.5f;
+                //Vector3.Scale(snowball.transform.localScale, snowball.transform.localScale + new Vector3(1.5f, 1.5f, 1.5f));
+            }
+            
         }
         else {
             anim.SetInteger("Walk", 0);
