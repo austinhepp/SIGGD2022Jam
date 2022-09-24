861921b41dce0cf332eb390406d63e1717be7df8
austinhepp <hepp0@purdue.edu>
2022-09-24 13:27:16 -0400

    fixed snowball

--- Assets/Scenes/snowball.cs
+++ Assets/Scenes/snowball.cs
@@ -6,6 +6,6 @@ using UnityEngine;
 public class snowball : MonoBehaviour {
     [SerializeField] private GameObject mTarget;
     void Update() {
-        transform.position = mTarget.transform.position + mTarget.transform.forward + Vector3.up;
+        transform.position = mTarget.transform.position + Vector3.Scale(mTarget.transform.forward, transform.localScale + new Vector3(0.3f, 0.3f, 0.3f)) + Vector3.Scale(new Vector3(0, 0.5f, 0), transform.localScale);
     }
 }
